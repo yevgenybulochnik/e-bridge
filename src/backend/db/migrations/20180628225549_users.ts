777f2111d5fc8082b3448f38aece01ace3a433d7
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-07-08 21:24:08 -0700

    Migration create multiple tables

--- src/backend/db/migrations/20180628225549_users.ts
+++ src/backend/db/migrations/20180628225549_users.ts
@@ -1,15 +1,24 @@
 import * as Knex from "knex";
 
-exports.up = function (knex: Knex, Promise: Promise<any>) {
-  return knex.schema.createTable('users', (table) => {
-    table.increments('id');
-    table.string('firstname').notNullable();
-    table.string('lastname').notNullable();
-    table.string('email').unique().notNullable();
-    table.string('password').notNullable();
-  })
+exports.up = function (knex: Knex, promise: Promise<any>) {
+  return Promise.all([
+    knex.schema.createTable('users', (table) => {
+      table.increments('id');
+      table.string('firstname').notNullable();
+      table.string('lastname').notNullable();
+      table.string('email').unique().notNullable();
+      table.string('password').notNullable();
+    }),
+    knex.schema.createTable('admin_menu', (table) => {
+      table.string('menu_item').notNullable();
+      table.string('item_path').notNullable();
+    })
+  ])
 };
 
-exports.down = function (knex: Knex, Promise: Promise<any>) {
-  return knex.schema.dropTable('users');
+exports.down = function (knex: Knex, promise: Promise<any>) {
+  return Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('admin_menu'),
+  ])
 };
