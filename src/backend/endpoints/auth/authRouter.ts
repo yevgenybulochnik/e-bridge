dfa39600f788986c9de8e7f7543e90be694c7c0c
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-07-10 01:36:31 +0000

    AuthMiddleware initial setup

--- src/backend/endpoints/auth/authRouter.ts
+++ src/backend/endpoints/auth/authRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import * as controller from './authController';
+import authenticate from '../authMiddleware'
+
 const AuthRouter = Router()
 
-AuthRouter.post('/register', controller.registerUser)
+AuthRouter.post('/register', authenticate, controller.registerUser)
 AuthRouter.post('/login', controller.loginUser)
 
 export default AuthRouter
---
+++ src/backend/endpoints/authMiddleware.ts
@@ -0,0 +1,24 @@
+import * as jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+export function authenticate(req: Request, res: Response, next: NextFunction) {
+  if (!(req.headers && req.headers.authorization)) {
+    return res.status(400).json({
+      status: 'Invalid Headers'
+    })
+  }
+  const authHeader = req.headers.authorization
+  const token = authHeader.split(' ')[1]
+  console.log(token)
+  jwt.verify(token, process.env.TOKEN_SECRET, (err: any, payload: any) => {
+    if (err) {
+      return res.status(500).json({
+        status: 'error',
+        message: err.message
+      })
+    }
+    next()
+  })
+}
+
+export default authenticate
