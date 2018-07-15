b1477115d608bee632e967b481be81a392735ff6
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-07-14 22:04:39 -0700

    Login loading state, needs to fix change handler

--- src/frontend/app/features/login/smart/login.tsx
+++ src/frontend/app/features/login/smart/login.tsx
@@ -1,23 +1,63 @@
 import * as React from 'react';
 import * as CSSModules from 'react-css-modules';
+import axios from 'axios';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import LinearProgress from '@material-ui/core/LinearProgress';
 const styles = require('./login.sass')
 
-class Login extends React.Component<any, any> {
+interface LoginState {
+  isLoading: boolean;
+  email: string;
+  password: string;
+}
+
+class Login extends React.Component<any, LoginState> {
+  state: LoginState = {
+    isLoading: false,
+    email: 'test',
+    password: ''
+  }
+
+  handleChange(property: any, event: any): any {
+    console.log([property])
+  }
+
+  handleClick = () => {
+    this.setState({isLoading: !this.state.isLoading})
+    axios.post('/auth/login', {
+      email: this.state.email,
+      password: this.state.password
+    })
+    .then(res => {
+      console.log(res)
+    })
+  }
+
   render() {
+    const { handleChange, handleClick } = this
+    const { isLoading, email, password } = this.state
+
     return (
     <form styleName='login-container' action="">
       <div styleName='label'>E-Bridge Login</div>
       <TextField
         label='Input User Email'
         placeholder='Email'
+        value={email}
+        onChange={(e) => handleChange('email', e)}
       />
       <TextField
         label='Input User Password'
         placeholder='Password'
+        value={password}
       />
-      <Button >Submit</Button>
+      <Button disabled={isLoading} onClick={handleClick} >Submit</Button>
+      {isLoading ? (
+        <LinearProgress />
+      ) : (
+        <div styleName='progress-holder'></div>
+      )}
     </form>
     )
   }
--- src/frontend/unit.tsx
+++ src/frontend/unit.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import Login from './app/features/login/smart/login'
+
 ReactDOM.render(
   <div>
+    <Login />
   </div>,
   document.getElementById('unit')
 )
