23133413d40b8fa6c6286e6af038d561661ceb4e
Yevgeny Bulochnik <Yevgeny.bulochnik@gmail.com>
2018-08-03 18:16:35 -0700

    Update unit.tsx

--- src/frontend/unit.tsx
+++ src/frontend/unit.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import NavBar from './app/features/nav/smart/navbar'
-
 ReactDOM.render(
   <div style={{width: '80em', height: '41em', border: 'solid black 1px'}}>
-    <NavBar>
-      <div>test</div>
-    </NavBar>
   </div>,
   document.getElementById('unit')
 )
