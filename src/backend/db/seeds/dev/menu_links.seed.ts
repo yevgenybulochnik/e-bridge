b7304d6f3406c26e037957375787f0462f049fed
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-08-07 16:26:41 +0000

    Adjusted perm level for links

--- src/backend/db/seeds/dev/menu_links.seed.ts
+++ src/backend/db/seeds/dev/menu_links.seed.ts
@@ -1,9 +1,9 @@
 import * as Knex from 'knex';
 
 let links = [
-  {link_name: 'Designer', link_path: '/designer', link_type: 'general', permission_level: 2 },
-  {link_name: 'Dashboard', link_path: '/dashboard', link_type: 'general', permission_level: 1 },
-  {link_name: 'Register', link_path: '/register', link_type: 'admin', permission_level: 3 },
+  {link_name: 'Designer', link_path: '/designer', link_type: 'general', permission_level: 2},
+  {link_name: 'Dashboard', link_path: '/dashboard', link_type: 'general', permission_level: 3},
+  {link_name: 'Register', link_path: '/register', link_type: 'admin', permission_level: 1},
 ]
 
 export function seed(knex: Knex) {
