3477a14a5a497f8cf9fcdbc7f7430e8e55fbcc64
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-08-08 19:51:28 +0000

    Functions to generate nav menu on user roles

--- src/backend/models/users.ts
+++ src/backend/models/users.ts
@@ -1,6 +1,33 @@
 import { dbConn } from '../db/knex';
 import * as bcrypt from 'bcryptjs';
 
+export function getUserRole(userID: number) {
+  return dbConn('users')
+    .where({id: userID})
+    .select('role')
+    .first()
+    .then(user => user.role)
+}
+
+export function getPermissionLevel(userRole: string) {
+  return dbConn('roles')
+    .where({role: userRole})
+    .select('permission_level')
+    .first()
+    .then(role => role.permission_level)
+}
+
+export function getMenuLinks(level: number) {
+  return dbConn('menu_links')
+    .where('permission_level', '>=', level)
+}
+
+export function userMenuLinks(id: number) {
+  return getUserRole(id)
+    .then(role => getPermissionLevel(role))
+    .then(level => getMenuLinks(level))
+}
+
 export function getUserByEmail(email: string) {
   return dbConn('users')
     .where({email})
