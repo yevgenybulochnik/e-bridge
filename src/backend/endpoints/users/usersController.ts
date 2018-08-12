40fcd39b04fc21500eb4bf4dcb92af53016fb7a4
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-08-11 17:52:09 -0700

    Format links from db

--- src/backend/endpoints/users/usersController.ts
+++ src/backend/endpoints/users/usersController.ts
@@ -1,13 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import * as database from '../../models'
 
+export function formatLinks(menuLink: any[]) {
+  return menuLink.map((link, index) => {
+    return {linkName: link.link_name, path: link.link_path, type: link.link_type, isActive: false}
+  })
+}
+
 export function getUserMenu(req: Request, res: Response, next: NextFunction) {
   const { userID } = req.params
   return database.userMenuLinks(userID)
     .then(menuLinks => {
+      const adjustedLinks = formatLinks(menuLinks)
       res.status(200).json({
         status: 'success',
-        links: menuLinks
+        links: adjustedLinks
       })
     })
     .catch(err => {
