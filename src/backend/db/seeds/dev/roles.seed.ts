247f2a77dd113118c850d97896d58899d1a52428
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-08-16 18:57:28 +0000

    Adjusted users and role seeds

--- src/backend/db/seeds/dev/roles.seed.ts
+++ src/backend/db/seeds/dev/roles.seed.ts
@@ -5,7 +5,8 @@ export function seed(knex: Knex) {
     .then(() => {
       return knex('roles').insert([
         {role: 'admin', permission_level: 1},
-        {role: 'pharm', permission_level: 2}
+        {role: 'pharm', permission_level: 2},
+        {role: 'COS', permission_level: 3}
       ])
     })
 }
--- src/backend/db/seeds/dev/users_seed.ts
+++ src/backend/db/seeds/dev/users_seed.ts
@@ -13,12 +13,25 @@ export function seed(knex: Knex) {
       return bcrypt.hash(pass, 10)
     })
     .then((hash) => {
-      return knex('users').insert({
+      return knex('users').insert([{
         firstname: 'yevgeny',
         lastname: 'bulochnik',
         email: 'yevgeny.bulochnik@gmail.com',
         role: 'admin',
         password: hash
-      })
+      }, {
+        firstname: 'jane',
+        lastname: 'doe',
+        email: 'jane.doe@test.com',
+        role: 'pharm',
+        password: hash
+      }, {
+        firstname: 'john',
+        lastname: 'doe',
+        email: 'john.doe@test.com',
+        role: 'COS',
+        password: hash
+      }
+      ])
     })
 }
