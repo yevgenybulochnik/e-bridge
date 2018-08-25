f24508cbb9ab472d69ac5e8110f31eb89f3a3063
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-08-25 16:09:35 +0000

    Patient search input jsx, sass

--- src/frontend/app/views/designer/smart/patientsearchcomponent.tsx
+++ src/frontend/app/views/designer/smart/patientsearchcomponent.tsx
@@ -2,8 +2,11 @@ import * as React from 'react';
 import * as CSSModules from 'react-css-modules';
 const styles = require('./patientsearchcomponent.sass')
 
-interface PatientSearchState {
+import TextField from '@material-ui/core/TextField';
 
+interface PatientSearchState {
+  patientName: string;
+  patientDOB: any;
 }
 
 interface PatientSearchProps {
@@ -11,9 +14,38 @@ interface PatientSearchProps {
 }
 
 class PatientSearchComponent extends React.Component<PatientSearchProps, PatientSearchState> {
+  state: PatientSearchState  = {
+    patientName: '',
+    patientDOB: ''
+  }
+
+  handlePatientNameInput = (event: any) => {
+  }
+
+  handleDOBInput = (event: any) => {
+  }
+
   render() {
+    const { patientName, patientDOB } = this.state
+    const { handleDOBInput, handlePatientNameInput } = this
+
     return (
-      <div styleName='container'>Hello world!</div>
+      <div styleName='container'>
+          <TextField
+          label='Patient Name'
+          placeholder='Firstname Lastname'
+          InputProps={{className: styles.namelabel}}
+          value={patientName}
+          onChange={(e) => handlePatientNameInput(e)}
+          />
+          <TextField
+          label='DOB'
+          placeholder='MM/DD/YYYY'
+          InputProps={{className: styles.doblabel}}
+          value={patientDOB}
+          onChange={(e) => handleDOBInput(e)}
+          />
+      </div>
     )
   }
 }
